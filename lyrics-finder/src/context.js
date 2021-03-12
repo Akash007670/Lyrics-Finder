6afdd1fc3f4396b031691263ab27ab87ce8b17d5
Akash007670 <akaysharma894@gmail.com>
2021-03-12 22:32:57 +0530

    Added hooks to add tracks

--- lyrics-finder/src/context.js
+++ lyrics-finder/src/context.js
@@ -1,37 +1,33 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-const Context = React.createContext();
+export const Context = React.createContext();
 
-export class Provider extends Component {
-  state = {
-    track_list: [
-      { track: { track_name: "abc" } },
-      { track: { track_name: "123" } },
-    ],
-    heading: "Top 10 Tracks",
+export function ContextController({ children }) {
+  let intialState = {
+    track_list: [],
+    heading: "",
+    // dispatch: action => this.setState(state => reducer(state, action))
   };
 
-  componentDidMount() {
+  const [state, setState] = useState(intialState);
+
+  useEffect(() => {
     axios
       .get(
         `https://cors-access-allow.herokuapp.com/https://api.musixmatch.com/ws/1.1/chart.tracks.get?page=1&page_size=10&country=us&apikey=${process.env.REACT_APP_API_KEY}`
       )
       .then((res) => {
-        console.log(res.data);
+        // console.log(res.data);
+        setState({
+          track_list: res.data.message.body.track_list,
+          heading: "Top 10 Tracks",
+        });
       })
-      .catch((err) => {
-        console.log(err);
-      });
-  }
+      .catch((err) => console.log(err));
+  }, []);
 
-  render() {
-    return (
-      <Context.Provider value={this.state}>
-        {this.props.children}
-      </Context.Provider>
-    );
-  }
+  return (
+    <Context.Provider value={[state, setState]}>{children}</Context.Provider>
+  );
 }
-
-export const Consumer = Context.Consumer;
