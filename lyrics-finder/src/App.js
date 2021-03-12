0aa4a6baa0045ff278ca298680a38a2a1faf68a3
Akash007670 <akaysharma894@gmail.com>
2021-03-12 22:34:13 +0530

    Added Context and switch path

--- lyrics-finder/src/App.js
+++ lyrics-finder/src/App.js
@@ -3,23 +3,23 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Index from "./components/layout/Index";
-import { Provider } from "./context";
+import { ContextController } from "./context";
 
-function App() {
+const App = () => {
   return (
-    <Provider>
+    <ContextController>
       <Router>
-        <React.Fragment>
+        <>
           <Navbar />
-        </React.Fragment>
-        <div className="container">
-          <Switch>
-            <Route exact path="/" component={Index} />
-          </Switch>
-        </div>
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Index} />
+            </Switch>
+          </div>
+        </>
       </Router>
-    </Provider>
+    </ContextController>
   );
-}
+};
 
 export default App;
